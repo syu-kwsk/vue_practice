3b6705ae0d88872ab5998b8335e12b57410637f9
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-12-31 00:16:42 +0900

    Add button to show and hide message

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'hello, world',
-    now: new Date().toLocaleString()
+    button_message: 'Hey!',
+    now: new Date().toLocaleString(),
+    seen: false
+  },
+  methods: {
+    hello: function() {
+      if (this.seen) {
+        this.button_message = 'Again!'
+      }
+      else {
+        this.button_message = 'Cancel'
+      }
+      this.seen = !(this.seen)
+    }
   }
 })
