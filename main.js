732ad615052ec99eb4f5f65550dce45ebfd92b0c
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-04-08 01:55:51 +0900

    Add filter for price-label

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 });
+
+var app1 = new Vue({
+  el: '#app1',
+  data: {
+    price: 1000
+  },
+  filters: {
+    number_format: function(val) {
+      return val.toLocaleString();
+    },
+    unit: function(val) {
+      return val + '円';
+    }
+  }
+});
